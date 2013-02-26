8e178e9918c99a6c8c1ade1a9dcd56df61fab59f
Karlit0 <antonio.stipanovic@fer.hr>
2013-02-26 14:24:13 +0100

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NastavaApp::Application.config.secret_token = 'd8d071327163c534a529c206608cb8b7df42bc9986c3e6c8ed8473b40e4c6d63765b2a38cbae722ead0aa9e2b52e81399e278eaaf8b137ff481cbdf0fa44fbbe'
